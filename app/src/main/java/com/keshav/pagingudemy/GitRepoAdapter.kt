a4e1d000dac2ae45b8fe1645d569a7f86e8b6a6f
HP <keshav.pai87@gmail.com>
2022-06-20 09:22:12 +0530

    Added the adapter class and the kotlin synthetic class extension in gradle

--- app/src/main/java/com/keshav/pagingudemy/GitRepoAdapter.kt
+++ app/src/main/java/com/keshav/pagingudemy/GitRepoAdapter.kt
@@ -1,4 +1,46 @@
 package com.keshav.pagingudemy
 
-class GitRepoAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class GitRepoAdapter : PagedListAdapter<GitRepo, GitRepoAdapter.RepoViewHolder>(REPO_COMPARATOR) {
+
+    class RepoViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
+        fun setData(getRepo : GitRepo) {
+            view.repoName.text = getRepo.fullName
+            view.repoDescription.text = getRepo.description
+            view.repoLanguage.text = getRepo.language
+            view.repo_forks.text = getRepo.forks.toString()
+            view.repo_stars.text = getRepo.stars.toString()
+        }
+    }
+
+    override fun onCreateViewHolder( parent: ViewGroup, viewType: Int ): GitRepoAdapter.RepoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return RepoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: GitRepoAdapter.RepoViewHolder, position: Int) {
+        val repo = getItem(position)
+        repo?.let {
+            holder.setData(repo)
+        }
+    }
+
+    companion object {
+        private val REPO_COMPARATOR = object : DiffUtil.ItemCallback<GitRepo>() {
+            override fun areItemsTheSame(oldGitRepo: GitRepo, newGitRepo: GitRepo): Boolean =
+                oldGitRepo.fullName == newGitRepo.fullName
+
+            @SuppressLint("DiffUtilEquals")
+            override fun areContentsTheSame(oldGitRepo: GitRepo, newGitRepo: GitRepo): Boolean =
+                oldGitRepo == newGitRepo
+        }
+    }
 }
